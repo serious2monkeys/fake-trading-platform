262f94b57ca7d437d103f8dee4634d11b4f4b16f
antondoronin <doronantonin@gmail.com>
2020-04-05 18:27:55 +0500

    blueprint UI version

--- trading_ui/src/pages/App.vue
+++ trading_ui/src/pages/App.vue
@@ -4,7 +4,30 @@
             <v-container>
                 <Header/>
                 <v-card>
-                    <trading-vue :data="chart" :height="this.height" :width="this.width" style="margin-top: 56px"/>
+                    <v-tabs fixed-tabs style="margin-top: 56px" v-model="tab">
+                        <v-tab>
+                            <v-icon>multiline_chart</v-icon>
+                            Coinbase
+                        </v-tab>
+                        <v-tab>
+                            <v-icon>show_chart</v-icon>
+                            Kraken
+                        </v-tab>
+
+                        <v-tab-item>
+                            <trading-vue :color-back="colors.colorBack"
+                                         :color-grid="colors.colorGrid"
+                                         :color-text="colors.colorText"
+                                         :data="dc"
+                                         :height="this.height"
+                                         :width="this.width"
+                                         style="margin-top: 56px"/>
+
+                        </v-tab-item>
+                        <v-tab-item>
+
+                        </v-tab-item>
+                    </v-tabs>
                 </v-card>
             </v-container>
         </v-content>
@@ -13,7 +36,8 @@
 
 <script>
     import TradingVue from 'trading-vue-js'
-    import Header from "../components/Header.vue";
+    import Header from "../components/Header.vue"
+    import DataCube from "trading-vue-js/src/helpers/datacube";
 
     export default {
         name: 'App',
@@ -23,51 +47,91 @@
         },
         methods: {
             onResize() {
-                this.width = window.innerWidth * 0.9;
-                this.height = window.innerHeight * 0.9
+                this.width = window.innerWidth * 0.8;
+                this.height = window.innerHeight * 0.7
             }
         },
         mounted() {
-            window.addEventListener('resize', this.onResize)
+            window.addEventListener('resize', this.onResize);
+            let filterFun = (tickData) =>
+                (tickData.payload.pair.baseCurrency == 'BTC'
+                    && tickData.payload.pair.targetCurrency == 'USD');
+            this.socket = new WebSocket("ws://localhost:8080/streaming");
+            this.socket.onopen = () => {
+                this.socket.onmessage = ({data}) => {
+                    let tick = JSON.parse(data);
+
+                    if (tick.exchange == 'COINBASE' && filterFun(tick)) {
+                        this.messagesCount++;
+                        let rate = tick.payload.rate;
+                        let timestamp = Math.round(tick.timestamp / 1000) * 1000;
+                        this.rateSum += rate;
+                        if (this.messagesCount < 2) {
+                            this.initialChart.push([timestamp, rate, rate, rate, rate, 0])
+                        } else if (this.messagesCount == 2) {
+                            console.log(this.initialChart);
+                            this.dc = new DataCube({
+                                chart: {
+                                    type: "Spline",
+                                    data: this.initialChart,
+                                    settings: {
+                                        lineWidth: 1.5
+                                    }
+                                }, onchart: [
+                                    {
+                                        name: "EMA, 5",
+                                        type: "EMA",
+                                        data: []
+                                    }
+                                ], offchart: []
+                            })
+                        } else {
+                            if (this.messagesCount < 5) {
+                                this.dc.update({
+                                    candle: [timestamp, rate, rate, rate, rate, 0]
+                                });
+                            } else {
+                                let emaValue = 0;
+                                if (this.messagesCount == 5) {
+                                    emaValue = this.rateSum / 5.0;
+                                } else {
+                                    emaValue = this.previousEma * 0.3 + rate * 0.7;
+                                }
+                                this.previousEma = emaValue;
+                                this.dc.update({
+                                    candle: [timestamp, rate, rate, rate, rate, 0],
+                                    'EMA, 5': emaValue
+                                });
+                            }
+                        }
+                    }
+                }
+            }
         },
         beforeDestroy() {
-            window.removeEventListener('resize', this.onResize)
+            window.removeEventListener('resize', this.onResize);
+            this.socket.close()
         },
         data() {
             return {
-                width: window.innerWidth * 0.9,
-                height: window.innerHeight * 0.9,
-                chart: {
-                    ohlcv: [
-                    ]
+                tab: null,
+                messagesCount: 0,
+                initialChart: [],
+                previousEma: 0,
+                rateSum: 0,
+                dc: new DataCube({
+                    chart: {
+                        type: "Spline",
+                        data: []
+                    }, onchart: [], offchart: []
+                }),
+                width: window.innerWidth * 0.8,
+                height: window.innerHeight * 0.7,
+                colors: {
+                    colorBack: '#fff',
+                    colorGrid: '#eee',
+                    colorText: '#333',
                 },
-                overlays: [{
-                    "name": "Trades",
-                    "type": "PerfectTrades",
-                    "data": [
-                        [
-                            new Date().getMilliseconds- 50000, // timestamp (then trade occured)
-                            1,             // state: 0 = idle, 1 = long
-                            3973.0         // filled price
-                        ],
-                        [
-                            new Date().getMilliseconds - 40000,
-                            0,
-                            4011.0
-                        ],
-                        [
-                            new Date().getMilliseconds - 30000, // This is our buy
-                            1,
-                            4038.0
-                        ],
-                        [
-                            new Date().getMilliseconds - 20000, // And this is obviously a sell
-                            0,
-                            4124.0
-                        ]
-                    ],
-                    settings: {}
-                }]
             }
         }
     }
